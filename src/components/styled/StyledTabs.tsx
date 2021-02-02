1f5c7efab4a7a34f3ad34e9a8a0562851e37b8af
Wael-Hx <wael.lhb@gmail.com>
2021-02-02 14:20:17 +0100

    tabs

---
+++ src/components/styled/StyledTabs.tsx
@@ -0,0 +1,30 @@
+import { makeStyles, Tabs, TabsProps } from "@material-ui/core";
+import { FC } from "react";
+
+const useStyles = makeStyles({
+  tabs: {
+    marginTop: "7%",
+  },
+  indicator: {
+    backgroundColor: "#1890ff",
+  },
+});
+
+interface STProps extends TabsProps {
+  topMargin?: string;
+}
+
+const StyledTabs: FC<STProps> = ({ children, ...props }) => {
+  const classes = useStyles({ topMargin: props.topMargin });
+  return (
+    <Tabs
+      className={classes.tabs}
+      classes={{ indicator: classes.indicator }}
+      {...props}
+    >
+      {children}
+    </Tabs>
+  );
+};
+
+export default StyledTabs;
