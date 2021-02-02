fbe07d594f53d41756ab921724b36ab604ddfa9c
Wael-Hx <wael.lhb@gmail.com>
2021-02-02 14:20:41 +0100

    single tab styled

---
+++ src/components/styled/StyledTab.tsx
@@ -0,0 +1,28 @@
+import { makeStyles, Theme, Tab, TabProps } from "@material-ui/core";
+import { FC } from "react";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  tab: {
+    textTransform: "none",
+    minWidth: 72,
+    fontWeight: theme.typography.fontWeightRegular,
+    fontFamily: "Raleway, Roboto, Segoe UI",
+    "&:hover": {
+      opacity: 1,
+    },
+    "&$selected": {
+      backgroundColor: "rgb(40, 44, 82)",
+    },
+    "&:focus": {
+      backgroundColor: "rgb(40, 44, 82)",
+    },
+  },
+  selected: {},
+}));
+
+const StyledTab: FC<TabProps> = ({ ...props }) => {
+  const classes = useStyles();
+  return <Tab className={classes.tab} {...props} />;
+};
+
+export default StyledTab;
