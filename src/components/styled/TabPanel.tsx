2c561b6f933d1105757b8b6317d91f6dd0db58dc
Wael-Hx <wael.lhb@gmail.com>
2021-02-02 14:19:48 +0100

    tab panel

---
+++ src/components/styled/TabPanel.tsx
@@ -0,0 +1,34 @@
+import { makeStyles } from "@material-ui/core";
+import { FC, ReactNode } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  tab: {
+    marginTop: "7%",
+    width: "75%",
+    height: "100%",
+  },
+}));
+
+interface TabPanelProps {
+  children?: ReactNode;
+  index: any;
+  value: any;
+}
+
+const TabPanel: FC<TabPanelProps> = ({ children, value, index, ...other }) => {
+  const classes = useStyles();
+  return (
+    <div
+      className={classes.tab}
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <>{children}</>}
+    </div>
+  );
+};
+
+export default TabPanel;
