b2f1e3205da1b35aec078d38d737d36b8a4a1c0d
Wael-Hx <wael.lhb@gmail.com>
2021-02-04 18:54:49 +0100

    global theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#121212",
+    },
+    secondary: {
+      main: "#1E1E1E",
+      dark: "#121212",
+    },
+    text: {
+      primary: "#E2E2E2",
+    },
+  },
+});
