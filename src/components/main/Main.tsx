fc55ad0872e908b2b51a50c394c9649494620467
Wael-Hx <wael.lhb@gmail.com>
2021-02-04 22:05:54 +0100

    box shadow

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,12 +5,12 @@ import CommunitySideBar from "./CommunitySideBar";
 
 const Main = () => {
   return (
-    <AnimatedContainer component="main" height="95%" width="97%" elevation={1}>
-      <PaperContainer width="25%" height="100%" elevation={0}>
+    <AnimatedContainer component="main" height="95%" width="97%" elevation={0}>
+      <PaperContainer width="25%" height="100%" elevation={2}>
         <CommunitySideBar />
       </PaperContainer>
       <Chat />
-      <PaperContainer width="25%" height="100%" elevation={0}></PaperContainer>
+      <PaperContainer width="25%" height="100%" elevation={2}></PaperContainer>
     </AnimatedContainer>
   );
 };
