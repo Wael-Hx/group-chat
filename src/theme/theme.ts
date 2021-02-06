45a74407e1235f6695fc4099eb6a381278648705
Wael-Hx <wael.lhb@gmail.com>
2021-02-06 04:44:35 +0100

    add custom palette props

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,16 +3,32 @@ import { createMuiTheme } from "@material-ui/core";
 export const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#ffffff",
-      dark: "#3f51b5",
-      light: "#0288d1",
+      main: "#1976d2",
+      dark: "#115293",
+      light: "#4791db",
+      contrastText: "#fff",
     },
     secondary: {
-      main: "#272343",
-      dark: "#0B1024",
+      main: "#fff",
+      dark: "#fff",
     },
     text: {
-      primary: "#121212",
+      primary: "#ffffff",
+    },
+    bg: {
+      main: "#2A3458",
+      light: "#2C70F5",
+      dark: "#202636",
+      contrastText: "#fff",
     },
   },
 });
+
+declare module "@material-ui/core/styles/createPalette" {
+  interface Palette {
+    bg: Palette["primary"];
+  }
+  interface PaletteOptions {
+    bg: PaletteOptions["primary"];
+  }
+}
