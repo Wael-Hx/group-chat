d143fbad11c0b7c2b1cea4b9d65b9aed2c72624f
Wael-Hx <wael.lhb@gmail.com>
2021-02-16 15:53:39 +0100

    display incoming message per community group

--- src/components/main/Chat.tsx
+++ src/components/main/Chat.tsx
@@ -1,7 +1,33 @@
+import { useReactiveVar } from "@apollo/client";
+import { chatMessagesTree } from "../../cache";
 import IconInput from "../styled/IconInput";
+import Msg from "./Msg";
 
-const Chat = () => {
-  return <IconInput />;
+const Chat = ({ currentChat }: ChatProps) => {
+  const chatState = useReactiveVar(chatMessagesTree);
+
+  if (!currentChat) {
+    return (
+      <div className="dev center">
+        <h1> click on the general section or create a new group</h1>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <section className="scroll">
+        {chatState.chats[currentChat]?.map((msg) => (
+          <Msg key={msg.username} message={msg} />
+        ))}
+      </section>
+      <IconInput />
+    </>
+  );
 };
 
 export default Chat;
+
+interface ChatProps {
+  currentChat: string | null;
+}
