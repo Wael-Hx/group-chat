4a5dcd01ffb470ad6cab79d62e4cb70f4310b8a4
Wael-Hx <wael.lhb@gmail.com>
2021-02-17 12:19:29 +0100

    get sender in from subscription

--- src/gql/subscriptions/chat.ts
+++ src/gql/subscriptions/chat.ts
@@ -3,6 +3,7 @@ import { gql } from "@apollo/client";
 export const MYSUBS = gql`
   subscription Messages($mySubs: [String!]!) {
     messages(mySubs: $mySubs) {
+      id
       username
       body
       timestamp
