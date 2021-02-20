4283cec0fcb9348dbc838b8c01956f429dff148f
Wael-Hx <wael.lhb@gmail.com>
2021-02-20 13:14:54 +0100

    group about / description

--- src/components/main/ChatSections.tsx
+++ src/components/main/ChatSections.tsx
@@ -1,81 +1,28 @@
-import { useState } from "react";
-import { makeStyles, ToggleButton, ToggleButtonGroup } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import PaperContainer from "../styled/PaperContainer";
 
-const useStyles = makeStyles((theme) => ({
-  toggleRoot: {
-    padding: 7,
-    height: 30,
-    width: "100%",
-    margin: "7px 0",
-    fontSize: "1rem",
-    color: "white",
-  },
-  label: {
-    height: "inherit",
-  },
-  group: {
-    marginTop: theme.spacing(5),
-    width: "100%",
-  },
-  selected: {
-    color: "white !important",
-  },
-}));
-
-const ChatSections = () => {
-  const [section, setSection] = useState<string | null>("main");
-  const classes = useStyles();
-  const changeSection = (
-    _: React.MouseEvent<HTMLElement>,
-    newSection: string | null
-  ) => {
-    setSection(newSection);
-  };
-
+const ChatSections = ({ description }: SectionProps) => {
   return (
-    <>
-      <ToggleButtonGroup
-        className={classes.group}
-        value={section}
-        orientation="vertical"
-        exclusive
-        onChange={changeSection}
-        aria-label="text alignment"
-      >
-        <ToggleButton
-          classes={{
-            root: classes.toggleRoot,
-            label: classes.label,
-            selected: classes.selected,
-          }}
-          value="main"
-        >
-          <h6> main </h6>
-        </ToggleButton>
-        <ToggleButton
-          classes={{
-            root: classes.toggleRoot,
-            label: classes.label,
-            selected: classes.selected,
-          }}
-          value="new stuff"
-        >
-          <h6> new stuff </h6>
-        </ToggleButton>
-        <ToggleButton
-          classes={{
-            root: classes.toggleRoot,
-            label: classes.label,
-            selected: classes.selected,
-          }}
-          value="other"
-          aria-label="other"
-        >
-          <h6> other </h6>
-        </ToggleButton>
-      </ToggleButtonGroup>
-    </>
+    <PaperContainer
+      width="100%"
+      flexDirection="column"
+      elevation={0}
+      center
+      backgroundColor="dark"
+      topMargin="10px"
+    >
+      <Typography variant="h5" gutterBottom>
+        About
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        {description ?? "no description provided"}
+      </Typography>
+    </PaperContainer>
   );
 };
 
 export default ChatSections;
+
+interface SectionProps {
+  description?: string;
+}
--- src/components/main/CommunitySideBar.tsx
+++ src/components/main/CommunitySideBar.tsx
@@ -102,7 +102,7 @@ const CommunitySideBar = ({ communityTabs }: CommunitiesData) => {
               )}`
             }
           />
-          <ChatSections />
+          <ChatSections description={comm.description} />
         </TabPanel>
       ))}
     </>
